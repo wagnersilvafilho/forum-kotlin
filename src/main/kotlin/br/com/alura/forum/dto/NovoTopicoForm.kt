42397767fb169f3916ab4aa413e733886b1b3cb1
wagnersilvafilho <wagnersf97@gmail.com>
2021-06-21 19:27:25 -0300

    Criado tratamento de erros, correção de mensagems 'trace'

--- src/main/kotlin/br/com/alura/forum/dto/NovoTopicoForm.kt
+++ src/main/kotlin/br/com/alura/forum/dto/NovoTopicoForm.kt
@@ -5,9 +5,10 @@ import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
 data class NovoTopicoForm (
-    @field:NotBlank @field:Size(min = 5, max = 100)
+    @field:NotBlank (message = "Titulo não pode ser em branco")
+    @field:Size(min = 5, max = 100, message = "Titulo deve ter entre 5 e 100 caracteres.")
     val titulo: String,
-    @field:NotBlank
+    @field:NotBlank (message = "Mensagem não pode ser em branco")
     val mensagem: String,
     @field:NotNull
     val idCurso: Long,
--- src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -3,6 +3,7 @@ package br.com.alura.forum.service
 import br.com.alura.forum.dto.AtualizacaoTopicoForm
 import br.com.alura.forum.dto.NovoTopicoForm
 import br.com.alura.forum.dto.TopicoView
+import br.com.alura.forum.exception.NotFoundException
 import br.com.alura.forum.mapper.TopicoFormMapper
 import br.com.alura.forum.mapper.TopicoViewMapper
 import br.com.alura.forum.model.Topico
@@ -13,7 +14,8 @@ import java.util.stream.Collectors
 @Service
 class TopicoService(private var topicos: List<Topico> = ArrayList(),
                     private val topicoViewMapper: TopicoViewMapper,
-                    private val topicoFormMapper: TopicoFormMapper
+                    private val topicoFormMapper: TopicoFormMapper,
+                    private val notFoundMessage: String = "Topico não encontrado"
 ){
 
 //    init {
@@ -72,7 +74,7 @@ class TopicoService(private var topicos: List<Topico> = ArrayList(),
     fun buscarPorId(id: Long): TopicoView{
         val topico = topicos.stream().filter { t ->
             t.id == id
-        }.findFirst().get()
+        }.findFirst().orElseThrow{NotFoundException(notFoundMessage)}
         return topicoViewMapper.map(topico)
     }
 
@@ -86,7 +88,7 @@ class TopicoService(private var topicos: List<Topico> = ArrayList(),
     fun atualizar(form: AtualizacaoTopicoForm): TopicoView {
         val topico = topicos.stream().filter { t ->
             t.id == form.id
-        }.findFirst().get()
+        }.findFirst().orElseThrow{NotFoundException(notFoundMessage)}
         val topicoAtualizado = Topico(
             id = form.id,
             titulo = form.titulo,
@@ -104,7 +106,7 @@ class TopicoService(private var topicos: List<Topico> = ArrayList(),
     fun deletar(id: Long) {
         val topico = topicos.stream().filter { t ->
             t.id == id
-        }.findFirst().get()
+        }.findFirst().orElseThrow{NotFoundException(notFoundMessage)}
         topicos = topicos.minus(topico)
     }
 
